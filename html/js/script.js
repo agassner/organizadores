464fbcda4df82c103059d9188719df58da080ed6
Arthur Gassner <arthur.gassner@gmail.com>
2010-02-23 23:21:52 +0100

    Added a js function to load the box content.

--- html/js/script.js
+++ html/js/script.js
@@ -124,7 +124,23 @@ function slideProduct(direction) {
     }
 }
 
+function loadHighlightsBoxContent() {
+    var highlightVertical = $("#highlight-vertical-1");
+    var url = "http://localhost:8000/products/highlights/box/";
+
+    $.get(url, function(data) {
+        //$.each(data.fields, function(i, item) {
+        //    $("<img/>").attr("src", item.media.m).appendTo("#images");
+        //    if ( i == 3 ) return false;
+        //});
+        //console.debug(data);
+    });
+
+}
+
 $(document).ready(function() {
 	startStopSlider();
     loadSlideProduct();
+    loadHighlightsBoxContent();
 });
+
